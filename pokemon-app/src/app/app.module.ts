2d30c9e48887ef09643a5c6eb387ba402f63177a
cparradoc <cparradoc@minsait.com>
2022-02-24 11:01:31 +0100

    main components created

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonHomeComponent } from './pokemon-home/pokemon-home.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonHomeComponent,
+    PokemonListComponent,
+    PokemonDetailComponent
   ],
   imports: [
     BrowserModule,
