9a258f62c02d59700b851295b96d1a86aea20de5
cparradoc <cparradoc@minsait.com>
2022-02-24 18:20:17 +0100

    pokemon list component functionality first try

--- pokemon-app/src/app/pokemon-list/pokemon-list.component.ts
+++ pokemon-app/src/app/pokemon-list/pokemon-list.component.ts
@@ -9,10 +9,54 @@ import { RequestService } from './services/request.service';
 })
 export class PokemonListComponent implements OnInit {
 
-  PokemonResponseInterface: PokemonResponseInterface;
-  constructor() { }
+  PokemonResponseInterface: PokemonResponseInterface; //resultado
+  pokemonList: PokemonInterface[] = [];
+  baseURL: string = 'https://pokeapi.co/api/v2/pokemon/?limit=';
+  limit: number = 151; //primera generacion
+
+  //Inicializacion/llamada del servicio
+  constructor(private RequestService: RequestService) { 
+    this.RequestService.getCharacters(this.baseURL + this.limit).
+    subscribe( async (data: PokemonResponseInterface) => {
+      const results: PokemonInterface[] = data.results;
+      let pokemon = [];
+      for(let pokeIterable in results) {
+        let id = pokeIterable['url'].split('/');
+        await pokemon.push(await fetch(pokemon['url']).then(async response => response.json())
+        .then(async data => {
+          return { id: Number(id[id.length - 2]),
+                  name: pokemon['name'],
+                  url: pokemon['url'],
+                  image: data['sprites']['front_default']};
+        }))
+      }
+      this.pokemonList = pokemon;
+      console.log(this.pokemonList);
+    });
+  }
 
   ngOnInit(): void {
+    callRequestService(this.baseURL+'25');
   }
+}
+
+function callRequestService(url: string) {
+  //getCharacters oara guardar los resultados
+  this.RequestService.getCharacters(url).subscribe( async (data: PokemonResponseInterface) => {
+    const results: PokemonInterface[] = data.results;
+    let pokemon = [];
+      for(let pokeIterable in results) {
+        let id = pokeIterable['url'].split('/');
+        await pokemon.push(await fetch(pokemon['url']).then(async response => response.json())
+        .then(async data => {
+          return { id: Number(id[id.length - 2]),
+                  name: pokemon['name'],
+                  url: pokemon['url'],
+                  image: data['sprites']['front_default']};
+        }))
+      }
+      this.PokemonResponseInterface = data;
+      this.PokemonResponseInterface.results = pokemon;
+  });
 
 }
