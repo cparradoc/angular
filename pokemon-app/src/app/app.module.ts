264b6c06810f23b0aff3a5125395f4a4850869ec
cparradoc <cparradoc@minsait.com>
2022-02-24 12:08:36 +0100

    basic footer, nav component and images

--- pokemon-app/src/app/app.module.ts
+++ pokemon-app/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { PokemonHomeComponent } from './pokemon-home/pokemon-home.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PokemonHomeComponent,
     PokemonListComponent,
-    PokemonDetailComponent
+    PokemonDetailComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
