a2a1e05cafe04627d1d29ff1618e87b72d41056d
cparradoc <cparradoc@minsait.com>
2022-02-24 12:52:33 +0100

    basic routing

--- pokemon-app/src/app/app-routing.module.ts
+++ pokemon-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonHomeComponent } from './pokemon-home/pokemon-home.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PokemonHomeComponent},
+  {path: 'pokemon-list', component: PokemonListComponent},
+  {path: 'pokemon-detail', component: PokemonDetailComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
