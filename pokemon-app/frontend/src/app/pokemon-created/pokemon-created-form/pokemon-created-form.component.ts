8c8c7e4c855ee9700d6497a1d443ff7eabf625da
cparradoc <cparradoc@minsait.com>
2022-02-25 18:15:15 +0100

    backend

---
+++ pokemon-app/frontend/src/app/pokemon-created/pokemon-created-form/pokemon-created-form.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PokemonCreatedInterface } from '../models/pokemon.created.interface';
+import { compareType } from './custom-validator';
+
+@Component({
+  selector: 'app-pokemon-created-form',
+  templateUrl: './pokemon-created-form.component.html',
+  styleUrls: ['./pokemon-created-form.component.scss']
+})
+export class PokemonCreatedFormComponent implements OnInit {
+
+  public userRegisterForm: FormGroup = null;
+  public submitted: boolean = false;
+  
+  constructor(private formBuilder: FormBuilder) { 
+    // Podemos meter valores por defecto en las comillas
+    this.userRegisterForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(20)]],
+      frontImage: ['', [Validators.required, Validators.maxLength(200)]],
+      backImage: ['', [Validators.required, Validators.maxLength(200)]],
+      firstType: ['', [Validators.required]],
+      secondType: ['']
+    },
+    {
+      validator: compareType('firstType', 'secondType')
+    });
+  }
+
+  ngOnInit() {}
+
+  public onSubmit(): void {
+    this.submitted = true;
+
+    if(this.userRegisterForm.valid) {
+      let types: string[];
+      types.push(this.userRegisterForm.get('firstType').value);
+      let secondType = this.userRegisterForm.get('secondType').value;
+      if(secondType) { //caso en el que el pokemon tenga dos tipos
+        types.push(secondType);
+      }
+      const pokemon : PokemonCreatedInterface = {
+        id: '',
+        name: this.userRegisterForm.get('name').value,
+        frontImage: this.userRegisterForm.get('frontImage').value,
+        backImage: this.userRegisterForm.get('backImage').value,
+        types: types
+      }
+    }
+
+    this.userRegisterForm.reset();
+    this.submitted = false;
+  }
+
+}
