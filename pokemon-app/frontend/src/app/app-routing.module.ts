ac5814937be29309afc78be1bdc1a1809c6ff4e1
cparradoc <cparradoc@minsait.com>
2022-02-28 13:26:28 +0100

    pokemon mix

--- pokemon-app/frontend/src/app/app-routing.module.ts
+++ pokemon-app/frontend/src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { PokemonCreatedComponent } from './pokemon-created/pokemon-created.compo
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
 import { PokemonHomeComponent } from './pokemon-home/pokemon-home.component';
 import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
+import { PokemonMixComponent } from './pokemon-mix/pokemon-mix.component';
 
 const routes: Routes = [
   {path: "", component: PokemonHomeComponent},
   {path: "pokemon-list", component: PokemonListComponent},
   {path: "pokemon/:id", component: PokemonDetailComponent},
   {path: "pokemon-created", component: PokemonCreatedComponent},
-  {path: "pokemon-created/:id", component: PokemonCreatedDetailComponent}
+  {path: "pokemon-created/:id", component: PokemonCreatedDetailComponent},
+  {path: "pokemon-mix", component: PokemonMixComponent}
 
 ];
 
--- pokemon-app/frontend/src/app/pokemon-mix/pokemon-mix.component.ts
+++ pokemon-app/frontend/src/app/pokemon-mix/pokemon-mix.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonInterface, PokemonResponseInterface } from '../pokemon-list/models/pokemon.interface';
+import { RequestService } from '../services/request.service';
 
 @Component({
   selector: 'app-pokemon-mix',
@@ -7,9 +9,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonMixComponent implements OnInit {
 
-  constructor() { }
+  pokemonList: PokemonInterface[] = [];
+  pokemonFilteredList: PokemonInterface[] = []; //lista filtrada
+  filter: string = '';
+  baseURL: string = 'https://pokeapi.co/api/v2/pokemon/?limit=';
+  limit: number = 151; //primera generacion
 
-  ngOnInit(): void {
+  //pokemon reales
+  constructor(private requestService: RequestService) { 
+    this.requestService.getCharacters(this.baseURL+this.limit).subscribe(
+      async (data: PokemonResponseInterface) => {
+        const results: PokemonInterface[] = data.results;
+        for(let pokemon of results) {
+          let id = pokemon['url'].split('/');
+          this.pokemonList.push(await fetch(pokemon['url']).then(async response => response.json())
+            .then(async (data) => {
+              return { id: Number(id[id.length - 2]), name: pokemon['name'], url: pokemon['url'], image: data['sprites']['front_default'] };
+            }));
+        }
+      });
+
+    //pokemon creados
+    fetch("http://localhost:3000/pokemon-created")
+    .then( async response => await response.json())
+    .then(async data => {
+      for (let pokeIterable of data){
+        this.pokemonList.push({
+          id: pokeIterable.id,
+          name: pokeIterable.name,
+          url: '',
+          image: pokeIterable.frontImage,
+        });
+      }
+    });
+
+  }
+
+  async ngOnInit() {
   }
 
 }
