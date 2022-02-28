1a3ef921a90bd79bdfa5e59f1dc7f75005aab267
cparradoc <cparradoc@minsait.com>
2022-02-28 09:22:08 +0100

    pokemon created list body

--- pokemon-app/frontend/src/app/pokemon-created/pokemon-created-list/pokemon-created-list.component.ts
+++ pokemon-app/frontend/src/app/pokemon-created/pokemon-created-list/pokemon-created-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonCreatedInterface } from '../models/pokemon.created.interface';
 
 @Component({
   selector: 'app-pokemon-created-list',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonCreatedListComponent implements OnInit {
 
+  pokemonCreatedList: PokemonCreatedInterface[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    fetch("http://localhost:3000/pokemon")
+    .then(async response => response.json())
+    .then(async data => {
+      let pokemon: PokemonCreatedInterface;
+      for(let i of data){
+        pokemon = {
+          id: i.id,
+          name: i.name,
+          frontImage: i.frontImage,
+          backImage: i.backImage,
+          types: i.types,
+        };
+        this.pokemonCreatedList.push(pokemon);
+      }
+    });
   }
-
 }
