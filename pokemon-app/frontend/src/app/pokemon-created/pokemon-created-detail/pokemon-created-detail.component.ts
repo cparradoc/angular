3ca42c04a725a0d233df56f8350d713304c29622
cparradoc <cparradoc@minsait.com>
2022-02-28 09:54:57 +0100

    pokemon created detail constructor

--- pokemon-app/frontend/src/app/pokemon-created/pokemon-created-detail/pokemon-created-detail.component.ts
+++ pokemon-app/frontend/src/app/pokemon-created/pokemon-created-detail/pokemon-created-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonCreatedInterface } from '../models/pokemon.created.interface';
+import { compareType } from '../pokemon-created-form/custom-validator';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-pokemon-created-detail',
@@ -7,7 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PokemonCreatedDetailComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  types: string[] = [];
+  pokemon: PokemonCreatedInterface;
+
+  firstTypeList: string[] = ['normal', 'fire', 'water', 'electric', 'grass', 'ice', 'ground', 'flying', 'ghost', 'rock', 'fighting', 'poison', 'psychic', 'bug', 'dark', 'steel', 'dragon', 'fairy'];
+	secondTypeList: string[] = ['', 'normal', 'fire', 'water', 'electric', 'grass', 'ice', 'ground', 'flying', 'ghost', 'rock', 'fighting', 'poison', 'psychic', 'bug', 'dark', 'steel', 'dragon', 'fairy'];
+
+  public userRegisterForm: FormGroup = null;
+  public submitted: boolean = false;
+
+
+  constructor(private formBuilder: FormBuilder) {
+    this.userRegisterForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(20)]],
+      frontImage: ['', [Validators.required, Validators.maxLength(200)]],
+      backImage: ['', [Validators.required, Validators.maxLength(200)]],
+      firstType: ['', [Validators.required]],
+      secondType: ['']
+    },
+    {
+      validator: compareType('firstType', 'secondType')
+    });
+   }
 
   ngOnInit(): void {
   }
